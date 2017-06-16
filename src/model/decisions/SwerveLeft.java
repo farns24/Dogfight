c2eccdb8e0ab620884b235d1d23d25f8e158fca5
Michael Rhodes <michael@Michaels-MacBook-Pro.local>
2017-06-16 13:03:58 -0600

    swerving decisions

--- src/model/decisions/SwerveLeft.java
+++ src/model/decisions/SwerveLeft.java
@@ -27,12 +27,29 @@ public class SwerveLeft implements IDecision {
 
     @Override
     public GameState simulate(boolean isAlive, int fuelLeft, double damage, int ammoLeft, List<Enemy> enemies,
-                              List<Boundaries> bounds, List<Base> bases, List<Obstical> obsticals, Conditions conditions) {
-        GameState newState = new GameState(new SwerveLeft(plane, 5), isAlive, fuelLeft, damage, ammoLeft,
-                enemies, bounds, bases, obsticals, conditions);
+                              List<Boundaries> bounds, List<Base> bases, List<Obstical> obsticals, Conditions c) {
 
+        GameState newState = new GameState(new SwerveLeft(plane, 5), isAlive, fuelLeft, damage, ammoLeft,
+                enemies, bounds, bases, obsticals, c);
+
+        if (c.OUTOFFUEL() && c.INENEMYSIGHTS()) {
+            newState.setDamage(damage + .1);
+        } else if (c.ENEMYINSIGHTS()) {
+            //Move self
+            newState.getConditions().setENEMYINSIGHTS(false);
+        } else if (c.INENEMYSIGHTS()) {
+            //Move self
+            newState.getConditions().setINENEMYSIGHTS(false);
+        } else if (c.APPROACHINGOBSTACLE()) {
+            //Move self
+            newState.getConditions().setAPPROACHINGOBSTACLE(false);
+        } else if (c.SEARCHING()) {
+            //Test if enemy comes into sight and change accordingly
+        }
+        //Test if approaching obstacle
+        //Change enemies positions && change ENEMYINSIGHTS and INENEMYSIGHTS accordingly
         newState.setFuelLeft(fuelLeft - fuelConsumption);
-        if (newState.getFuelLeft() <= 0) {}
+
         return newState;
     }
 }
